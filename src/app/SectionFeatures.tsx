9749c3ef4588353f855c4083c08930b785280d37
roymandev <royman.dev@gmail.com>
2023-07-03 00:35:04 +0700

    feat(home): make features section responsive

--- src/app/SectionFeatures.tsx
+++ src/app/SectionFeatures.tsx
@@ -28,18 +28,22 @@ type CardProps = {
 };
 
 const Card = ({ icon, title, desc }: (typeof FEATURES)[number]) => (
-  <article className="flex flex-1 flex-col items-start rounded-[5px] bg-white/5 px-10 py-8">
-    <span className="rounded-[3px] bg-white/[.03] p-[13px]">
+  <article className="flex flex-1 items-start gap-4 rounded-[5px] bg-white/5 p-4 sm:flex-col sm:gap-7 sm:p-6 lg:px-10 lg:py-8">
+    <span className="shrink-0 rounded-[3px] bg-white/[.03] p-[13px]">
       <Image alt={`Icon ${title}`} src={icon} />
     </span>
-    <h3 className="mt-7 text-lg font-bold">{title}</h3>
-    <p className="mt-2 leading-8 text-white/60">{desc}</p>
+    <div>
+      <h3 className="text-lg font-bold">{title}</h3>
+      <p className="mt-2 leading-8 text-white/60 max-sm:text-sm max-sm:leading-6">
+        {desc}
+      </p>
+    </div>
   </article>
 );
 
 const SectionFeatures = () => {
   return (
-    <div className="mx-auto flex w-full max-w-5xl gap-5">
+    <div className="mx-auto flex w-full max-w-5xl gap-5 max-sm:flex-col">
       {FEATURES.map((feature) => (
         <Card key={feature.title} {...feature} />
       ))}
