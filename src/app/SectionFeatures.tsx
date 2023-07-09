a9dabd74e5d852e9fa520d2ff7fac7badb712214
roymandev <royman.dev@gmail.com>
2023-07-09 12:35:14 +0700

    feat: add feature card component

--- src/app/SectionFeatures.tsx
+++ src/app/SectionFeatures.tsx
@@ -1,9 +1,9 @@
 import IconFigma from '~/icon-figma.svg';
 import IconCode from '~/icon-code.svg';
 import IconBox from '~/icon-box.svg';
-import Image, { ImageProps } from 'next/image';
+import FeatureCard, { FeatureCardProps } from '@/components/FeatureCard';
 
-const FEATURES: CardProps[] = [
+const FEATURES: FeatureCardProps[] = [
   {
     icon: IconFigma,
     title: 'Design',
@@ -21,31 +21,11 @@ const FEATURES: CardProps[] = [
   },
 ];
 
-type CardProps = {
-  icon: ImageProps['src'];
-  title: string;
-  desc: string;
-};
-
-const Card = ({ icon, title, desc }: (typeof FEATURES)[number]) => (
-  <article className="flex flex-1 items-start gap-4 rounded-[5px] bg-white/5 p-4 sm:flex-col sm:gap-7 sm:p-6 lg:px-10 lg:py-8">
-    <span className="shrink-0 rounded-[3px] bg-white/[.03] p-[13px]">
-      <Image alt={`Icon ${title}`} src={icon} />
-    </span>
-    <div>
-      <h3 className="text-lg font-bold">{title}</h3>
-      <p className="mt-2 leading-8 text-white/60 max-sm:text-sm max-sm:leading-6">
-        {desc}
-      </p>
-    </div>
-  </article>
-);
-
 const SectionFeatures = () => {
   return (
     <div className="mx-auto flex w-full max-w-5xl gap-5 max-sm:flex-col">
       {FEATURES.map((feature) => (
-        <Card key={feature.title} {...feature} />
+        <FeatureCard key={feature.title} {...feature} iconStyle="squared" />
       ))}
     </div>
   );
---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,40 @@
+import twclsx from '@/lib/twclsx';
+import Image, { ImageProps } from 'next/image';
+
+export type FeatureCardProps = {
+  icon: ImageProps['src'];
+  title: string;
+  desc: string;
+  iconStyle?: 'default' | 'squared';
+};
+
+const FeatureCard = ({
+  icon,
+  title,
+  desc,
+  iconStyle = 'default',
+}: FeatureCardProps) => (
+  <article className="flex flex-1 items-start gap-4 rounded-[5px] bg-white/5 p-4 sm:flex-col sm:gap-7 sm:p-6 lg:px-10 lg:py-8">
+    <span
+      className={twclsx(
+        'shrink-0',
+        iconStyle === 'squared' && 'rounded-[3px] bg-white/[.03] p-[13px]',
+      )}
+    >
+      <Image
+        alt={`Icon ${title}`}
+        src={icon}
+        width={iconStyle === 'squared' ? 24 : 50}
+        height={iconStyle === 'squared' ? 24 : 50}
+      />
+    </span>
+    <div>
+      <h3 className="text-lg font-bold">{title}</h3>
+      <p className="mt-2 leading-8 text-white/60 max-sm:text-sm max-sm:leading-6">
+        {desc}
+      </p>
+    </div>
+  </article>
+);
+
+export default FeatureCard;
